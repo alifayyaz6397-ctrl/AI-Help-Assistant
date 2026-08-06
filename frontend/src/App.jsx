04b35b16d5e8dc8b1dfb6aeb8298ecc746ea1046
Ali Fayyaz <alifayyaz6397@gmail.com>
2026-08-06 14:59:16 +0500

    Change API_URL to an empty string

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useRef, useEffect } from "react";
 import "./App.css";
-export const API_URL = import.meta.env.VITE_API_URL;
+export const API_URL = "";
 
 function App() {
   const [query, setQuery] = useState("");
